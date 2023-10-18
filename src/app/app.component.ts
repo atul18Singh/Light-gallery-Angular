f99d30c8886106521816099151be72d5a75a7c68
atul18singh <atul18singh@gmail.com>
2023-10-18 10:35:50 +0530

    light gallery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
+import lgZoom from 'lightgallery/plugins/zoom';
+import lgRotate from 'lightgallery/plugins/rotate';
+import lgShare from 'lightgallery/plugins/share';
+import lgFullscreen from 'lightgallery/plugins/fullscreen';
+import { BeforeSlideDetail } from 'lightgallery/lg-events';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'light_gallery';
+  name = 'Angular ' + VERSION.major;
+  settings = {
+    counter: false,
+    plugins: [lgZoom, lgRotate, lgShare, lgFullscreen],
+    flipHorizontal: true,
+    rotatePluginStrings: {
+      flipVertical: 'Flip vertical',
+      flipHorizontal: 'Flip horizontal',
+      rotateLeft: 'Rotate left',
+      rotateRight: 'Rotate right'
+    },
+  };
+  onBeforeSlide = (detail: BeforeSlideDetail): void => {
+    const { index, prevIndex } = detail;
+    console.log(index, prevIndex);
+  };
 }
