51afdcfa4c76a79658afd571850700fa704a23e5
atul18singh <atul18singh@gmail.com>
2023-11-01 16:49:57 +0530

    get update light gallery dynamic now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,6 +23,58 @@ export class AppComponent {
       rotateRight: 'Rotate right'
     },
   };
+
+  items = [
+    {
+      id: '1',
+      size: '480-480-480, 800-800-800, 1400-1400',
+      src:
+        'https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80',
+      thumb:
+        'https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80',
+      subHtml: `<div class="lightGallery-captions">
+            <h4>Photo by <a href="https://unsplash.com/@dann">Dan</a></h4>
+            <p>Published on November 13, 2018</p>
+        </div>`
+    },
+    {
+      id: '2',
+      size: '480-480-480, 800-800-800, 1400-1400',
+      src:
+        'https://images.unsplash.com/photo-1473876988266-ca0860a443b8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80',
+      thumb:
+        'https://images.unsplash.com/photo-1473876988266-ca0860a443b8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80',
+      subHtml: `<div class="lightGallery-captions">
+            <h4>Photo by <a href="https://unsplash.com/@kylepyt">Kyle Peyton</a></h4>
+            <p>Published on September 14, 2016</p>
+        </div>`
+    },
+    {
+      id: '3',
+      size: '480-480-480, 800-800-800, 1400-1400',
+      src:
+        'https://images.unsplash.com/photo-1588953936179-d2a4734c5490?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1400&q=80',
+      thumb:
+        'https://images.unsplash.com/photo-1588953936179-d2a4734c5490?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=240&q=80',
+      subHtml: `<div class="lightGallery-captions">
+            <h4>Photo by <a href="https://unsplash.com/@jxnsartstudio">Garrett Jackson</a></h4>
+            <p>Published on May 8, 2020</p>
+        </div>`
+    },
+    {
+      id: '4',
+      size: '480-480-480, 800-800-800, 1400-1400',
+      src:
+        'https://images.unsplash.com/photo-1609902726285-00668009f004?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1400&q=80',
+      thumb:
+        'https://images.unsplash.com/photo-1609902726285-00668009f004?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=240&q=80',
+      subHtml: `<div class="lightGallery-captions">
+          <h4>Photo by <a href="https://unsplash.com/@bruno_adam">Bruno Adam</a></h4>
+          <p>Published on January 6, 2021</p>
+      </div>`
+    }
+  ];
+
   onBeforeSlide = (detail: BeforeSlideDetail): void => {
     const { index, prevIndex } = detail;
     console.log(index, prevIndex);
